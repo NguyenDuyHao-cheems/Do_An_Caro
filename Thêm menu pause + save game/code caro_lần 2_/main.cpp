a7cb3a96df09dbd114d93dfa2667cef1aa0e6cea
XD315 <xuanduy3105@gmail.com>
2024-12-01 15:10:33 +0700

    main.cpp

--- Thêm menu pause + save game/code caro_lần 2_/main.cpp
+++ Thêm menu pause + save game/code caro_lần 2_/main.cpp
@@ -1,13 +1,14 @@
 #include "Console.h"
 #include "Menu.h"
 #include "Game.h"
-//viet code vao day 
+
 int main() {
+    PlayMusic("musicc.wav", L"music_bg");
     SetConsoleOutputCP(437);
-    SetFontSize(7, 14);
-    setConsoleWindow(1600, 900);
+    SetFontSize(16, 16);
+    SetConsoleSize(146,39);
     FixConsoleWindow();
-    // PlayMusic(true);
+
     printMenu();
     system("pause");
     return 0;

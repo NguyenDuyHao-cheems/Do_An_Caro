200c90477c2b55463e9fbc404439bc5d7a991f9f
anhduc-1 <bnguyenanhduc@gmail.com>
2024-12-19 23:14:51 +0700

    Update game.h

--- Thêm menu pause + save game/code caro_lần 2_/game.h
+++ Thêm menu pause + save game/code caro_lần 2_/game.h
@@ -11,8 +11,10 @@ extern bool isMusicOn;
 extern int _COMMAND;
 void AskContinue();
 void Count_sumTime(TIME& time, int x, int y, int& k);
-void CountTime_XO(TIME& time, int x, int y, int& k);
-void PlayGame(int k);
+
+void CountTime_XO(TIME& time, int x, int y, int& k, int cnttime);
+
+void PlayGame(int k, int& win_x, int& win_y, int cnttime);
 bool checkWin(int row, int col, int winPositions[5][2]);
 bool isFull(_POINT board[][BOARD_SIZE]);
 int TestBoard(int row, int col, int winPositions[5][2]);
@@ -20,10 +22,11 @@ void MoveRight();
 void MoveLeft();
 void MoveDown();
 void MoveUp();
-void StartGame();
+void StartGame(int k, int  cnttime);
 
 void hideCursor();
 void showCursor();
+void cursorBot(int _X, int _Y, int& preX, int& preY);
 void changeColorCursor(bool turn);
 //load
 void loadGameState(char filename[]);
@@ -32,8 +35,9 @@ void LoadGame();
 //play with bot
 int evaluatePosition(int row, int col, int player);
 void BotMove(int& pX, int& pY);
-void PlaywithBot(int k);
-void StartGamewithbot();
+
+void PlaywithBot(int k, int& win_x, int& win_y, int cnttime);
+void StartGamewithbot(int k, int cnttime);
 void AskContinuePlaybot();
 
 void nhapnhay(const int winPositions[5][2], char symbol); //hieu ung noi bat chuoi lien tiep
@@ -41,10 +45,31 @@ void nhapnhay(const int winPositions[5][2], char symbol); //hieu ung noi bat chu
 void ResumeGame(int gameOption);
 void SaveGameName();
 bool checkDuplicate(char filename[]);
+
+SystemTime getSystemTime();
+void writeSystemTime();
+SystemTime readSystemTime(int numTimes);
+void TimeMagToArray();
+
+void getStats();
+void writeSumOfTime();
+Stats readSumOfTime(int numTimes);
+void statsToArray();
+
 int getNumSaveFile(char savefiles[][MAX_FILE_LENGTH + 1]);
 bool isValidName(char filename[]);
 void writeTempToSF();
+void confirmMenu();
+void deleteSaveFile(char* filename);
+void overwriteSF();
 
 void TableResult(int& win_x, int& win_y, int& run_x, int& run_y);
 void drawTableResult();
+void drawPhuthuy(int x, int y);
 
+void drawStart(int x, int y);
+
+void drawEnd(int x, int y);
+
+void SmallMenu(int x, int y);
+void drawPikachu(int x, int y);
